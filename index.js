e14095f23326b476abc83f6e4f1a2577b348eae3
Manish <manish.kini@magitt.com>
2022-06-04 15:47:12 +0530

    routes and controllers set

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const PORT = '9633';
 const app = express();
+// database connection
 const db = require('./config/mongoose.js');
 
 const bodyParser = require('body-parser');
 
+// passport jwt strategy for authentication
 const JWTStrategy = require('./config/passport-jwt-strategy');
 
 // body parser for parsing the response
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/', require());
 
 app.listen(PORT, (err) => {
   if (err) {
